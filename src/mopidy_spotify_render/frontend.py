0c43e9d8ac48423eabb6fffb6e51ff546a7e53e1
Varun Gujjar <varun.gujjar0406@gmail.com>
2025-04-23 19:14:53 +0200

    Elapsed Time & Event Handling
    Added elpased time timer
    Better event handling
    Bug fixes

--- src/mopidy_spotify_render/frontend.py
+++ src/mopidy_spotify_render/frontend.py
@@ -2,6 +2,7 @@ import logging
 import pykka
 import subprocess
 import os
+import time
 import json
 import threading
 from pathlib import Path
@@ -21,6 +22,11 @@ class SpotifyRenderFrontend(pykka.ThreadingActor, CoreListener):
         self.running = True
         self.last_event = ""
         self.uri= ""
+        self.track = None
+        self.timer = None
+        self.timer_running = True
+        self.timer_paused = False
+        self.elapsed_timer_count = 0
 
     def start_librespot(self):
         self.end_librespot()  # Ensure no duplicate process
@@ -38,17 +44,50 @@ class SpotifyRenderFrontend(pykka.ThreadingActor, CoreListener):
                 "--onevent", on_event_path,
                 "--initial-volume", self._config["spotify-render"]["initial-volume"],
                 "--device-type", self._config["spotify-render"]["device-type"],
+                "--device", self._config["spotify-render"]["device"]
             ]  
         subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
     def end_librespot(self):
+        logger.info("Spotify renderer: Stopped")
         subprocess.Popen(["killall","-s9", "librespot"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
 
+    # Elpased Time Timer
+    def pause_timer(self):
+        self.timer_paused = True
+
+    def resume_timer(self):
+        self.timer_paused = False
+
+    def stop_timer(self):
+        self.timer_running = False
+        self.timer_paused = False
+        self.elapsed_timer_count = 0
+        if self.timer and self.timer.is_alive():
+            self.timer.join()
+        self.timer = None
+
+    def start_timer(self):
+        self.timer_running = True
+        self.timer_paused = False
+        self.elapsed_timer_count = 0
+        self.timer = threading.Thread(target=self.elapsed_timer, daemon=True)
+        self.timer.start()
+
+    def elapsed_timer(self):
+        while self.timer_running:
+            if not self.timer_paused:
+                self.elapsed_timer_count += 1
+                self.core.playback.set_position(DurationMs(self.elapsed_timer_count * 1000))
+                time.sleep(1) 
+            else:
+                time.sleep(0.1)
+
     def on_event(self, event, **kwargs):
         """Handle Mopidy events"""
-        # print(f"Received event: {event}")
+    
 
     def on_start(self):
         threading.Thread(target=self.start_librespot, daemon=True).start()
@@ -74,32 +113,43 @@ class SpotifyRenderFrontend(pykka.ThreadingActor, CoreListener):
                     except json.JSONDecodeError as e:
                         logger.error(f"Error decoding JSON: {e}")
                         continue
-
+                    
+                    
                     if "player_event" in event and event["player_event"] != self.last_event:
                         self.last_event = event["player_event"]
 
                         if event["player_event"] == 'session_connected':
-                            self.core.playback.set_state(PlaybackState.STOPPED)
+                            self.core.playback.stop()
                             self.core.playback.set_metadata(None)
+                            CoreListener.send("options_changed", source='spotify-connect', state={'connected': True})
                             logger.info("Spotify renderer: Connected")
 
                         if event["player_event"] == 'session_disconnected':
-                            self.core.playback.set_state(PlaybackState.STOPPED)
+                            self.core.playback.stop()
                             self.core.playback.set_metadata(None)
                             self.core.playback.set_position(DurationMs(int(0)))
+                            self.stop_timer()
+                            CoreListener.send("options_changed", source='spotify-connect', state={'connected': False})
                             logger.info("Spotify renderer: Disconnected")
 
                         if event["player_event"] in ('seeked', 'position_correction', 'playing'):
                             self.core.playback.set_state(PlaybackState.PLAYING)
                             self.core.playback.set_position(DurationMs(int(event["position_ms"])))
+                            self.elapsed_timer_count = int(event["position_ms"]) / 1000
 
+                        if event["player_event"] == 'playing':
+                            self.resume_timer()
+                        
                         if event["player_event"] == 'paused':
                             self.core.playback.set_state(PlaybackState.PAUSED)
                             self.core.playback.set_position(DurationMs(int(event["position_ms"])))
+                            self.pause_timer()
 
                         if event["player_event"] == 'track_changed':
-                            logger.info("Spotify renderer: Track Changed")
-                            
+
+                            if self.track is not None:
+                                CoreListener.send("track_playback_ended", tl_track=self.track)
+                                self.stop_timer()
 
                             track_id = int(event["number"])
                             track = Track(
@@ -112,12 +162,17 @@ class SpotifyRenderFrontend(pykka.ThreadingActor, CoreListener):
                                 track_no = int(event["number"]) or None,
                                 disc_no = int(event["disc_number"]) or None,
                                 length = int(event["duration_ms"]),  
-                                bitrate= None,  
+                                bitrate= int(self._config["spotify-render"]["bitrate"]) * 1000,  
                                 comment=event["description"] or None,
                             )
+                            
 
                             tl_track = TlTrack(track_id, track=track)
+                            self.track = tl_track
                             self.core.playback.set_metadata(tl_track)
+                            CoreListener.send("track_playback_started", tl_track=tl_track)
+                            self.stop_timer()
+                            self.start_timer()
                             self.uri = event["uri"]
 
             except Exception as e:
@@ -127,4 +182,3 @@ class SpotifyRenderFrontend(pykka.ThreadingActor, CoreListener):
     def on_stop(self):
         self.running = False
         self.end_librespot()
-        logger.info("Librespot event listener stopped.")
